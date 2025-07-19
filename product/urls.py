22c251914b260a6036eabea39077f6db5e196c0c
rama alhayek <eng.ramaalhayek@gmail.com>
2025-07-19 09:48:53 +0300

    Add serializers & views for product, category

---
+++ product/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from product.views.category_views import CategoryViewSet
+from product.views.product_views import ProductViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
