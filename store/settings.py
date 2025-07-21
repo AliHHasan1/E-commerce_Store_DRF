6cb7120266346de89e25e675668fe454a4475ade
jesusabrast <rastessa5@gmail.com>
2025-07-21 11:43:20 +0300

    jwt from essa

--- store/settings.py
+++ store/settings.py
@@ -90,6 +90,9 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 8,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 
