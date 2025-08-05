c130b4b56cde8a73f6751d5db2cafcf5c39c3d5b
zhraaali <zhraa002.z.ali.2002@gmail.com>
2025-08-05 11:41:14 +0300

    dashboard

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
+# account/admin.py
 from django.contrib import admin
+from .models import Customer # استيراد نموذج Customer الخاص بك
 
-# Register your models here.
+# تسجيل نموذج Customer في لوحة الإدارة
+admin.site.register(Customer)
