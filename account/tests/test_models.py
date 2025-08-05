aa170bf809ac76ffb569626b54b515bb16292470
AliHHasan1 <eng.alihasan2@gmail.com>
2025-08-05 16:21:32 +0300

    Add sample data command and fix bugs

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -1,7 +1,7 @@
 import pytest
-from account.models import Account
+from account.models import Customer
 
 @pytest.mark.django_db
 def test_account_creation():
-    acc = Account.objects.create(username="essa", email="essa@example.com")
+    acc = Customer.objects.create(username="essa", email="essa@example.com")
     assert acc.username == "essa"
--- product/models/__init__.py
+++ product/models/__init__.py
@@ -1,2 +1,3 @@
 from .category_models import Category
 from .product_models import Product
+from .product_models import ProductReview
