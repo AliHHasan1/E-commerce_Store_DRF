40ffd8945971eb249c81c8c7ccf849bb4a82368e
jesusabrast <rastessa5@gmail.com>
2025-08-07 23:28:12 +0300

    merge between order & orderitem (just in serializer & views) and build readme file

--- order/tests/test_serializers.py
+++ order/tests/test_serializers.py
@@ -1,12 +1,23 @@
 import pytest
+from order.serializers.order_serializers import OrderItemSerializer
+from product.models import Product
 from order.models import Order
-from order.serializers import OrderSerializer
 from account.models import Customer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 @pytest.mark.django_db
-def test_order_serializer_fields():
-    account = Customer.objects.create(username="essa", email="e@e.com")
-    order = Order.objects.create(account=account, status="pending")
-    serializer = OrderSerializer(order)
-    data = serializer.data
-    assert data["status"] == "pending"
+def test_order_item_serializer_validation():
+    product = Product.objects.create(name="Mouse", price=50, stock_quantity=10, is_available=True)
+    data = {"product": product, "quantity": 5}
+    serializer = OrderItemSerializer(data=data)
+    assert serializer.is_valid()
+
+@pytest.mark.django_db
+def test_order_item_serializer_invalid_stock():
+    product = Product.objects.create(name="Mouse", price=50, stock_quantity=2, is_available=True)
+    data = {"product": product, "quantity": 5}
+    serializer = OrderItemSerializer(data=data)
+    assert not serializer.is_valid()
+    assert "المخزون غير كافٍ" in str(serializer.errors)
